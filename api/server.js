cfbee76b9fbbb2fed4166a78b7a0a4a5283305dc
kl2400031790 <yashaswikalyanam@gmail.com>
2026-02-27 08:38:37 +0530

    Fix React frontend serving and server setup

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
-import serverless from "serverless-http";
 import app from "../server/app.js";
 
-export default serverless(app);
+export default function handler(req, res) {
+  return app(req, res);
+}
